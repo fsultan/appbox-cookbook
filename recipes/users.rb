2cb7a373363c0c2d6430ceec827de1afdc772138
John Grayson <ajgrayson@gmail.com>
2013-12-02 19:35:08 +1300

    Update users.rb

--- recipes/users.rb
+++ recipes/users.rb
@@ -13,6 +13,7 @@ user_account node["appbox"]["apps_user"] do
 end
 user_account node["appbox"]["deploy_user"] do
   comment "deployer"
+  groups ['apps']
   ssh_keys node["appbox"]["deploy_keys"]
 end
 user_account node["appbox"]["admin_user"] do
@@ -29,20 +30,10 @@ group node["appbox"]["apps_user"] do
   ]
 end
 
-group node["appbox"]["deploy_user"] do
-  action :modify
-  members [
-    node["appbox"]["apps_user"],
-    node["appbox"]["admin_user"],
-    node["appbox"]["deploy_user"]
-  ]
-end
-
 node.default["authorization"]["sudo"]["groups"] = [
   "sudo",
   node["appbox"]["admin_user"],
-  node["appbox"]["deploy_user"],  # TODO workaround to enable deployer to restart app
-  node["appbox"]["apps_user"]  # TODO workaround so apps can deploy so sidekiq user has same rights as apps
+  node["appbox"]["deploy_user"]  # TODO workaround to enable deployer to restart app
 ]
 node.default["authorization"]["sudo"]["passwordless"] = true
 include_recipe "sudo"
